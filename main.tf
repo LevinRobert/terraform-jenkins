8e3a3839b0b70bf7ed2eb21d588550946c87baed
LevinRobert <levin16robert@gmail.com>
2026-01-11 21:27:44 +0530

    Reduce EC2 instance count from 3 to 2

--- main.tf
+++ main.tf
@@ -79,14 +79,14 @@ resource "aws_security_group" "web_sg" {
 # Launch EC2 instance in the same subnet & VPC
 # Launch multiple EC2 instances
 resource "aws_instance" "web" {
-  count                  = 3
+  count                  = 2
   ami                    = "ami-0f5ee92e2d63afc18"  # Amazon Linux 2 (ap-south-1)
   instance_type          = "t2.medium"
   subnet_id              = aws_subnet.my_subnet.id
   vpc_security_group_ids = [aws_security_group.web_sg.id]
 
   tags = {
-    Name = "ansible-${count.index}"
+    Name = "ansible"
   }
 }
 
