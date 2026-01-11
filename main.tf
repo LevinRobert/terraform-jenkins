f8b27e68f5722975995aa3992032222f7de1bce4
LevinRobert <levin16robert@gmail.com>
2026-01-11 20:44:33 +0530

    Update terraform config and add gitignore

---
+++ main.tf
@@ -0,0 +1,89 @@
+# Create a VPC
+resource "aws_vpc" "my_vpc" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "terraform-vpc"
+  }
+}
+
+# Create a Subnet in the VPC
+resource "aws_subnet" "my_subnet" {
+  vpc_id            = aws_vpc.my_vpc.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "ap-south-1a"
+  tags = {
+    Name = "terraform-subnet"
+  }
+}
+
+# Create an Internet Gateway for the VPC
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.my_vpc.id
+  tags = {
+    Name = "terraform-igw"
+  }
+}
+
+# Create a Route Table
+resource "aws_route_table" "route_table" {
+  vpc_id = aws_vpc.my_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "terraform-route-table"
+  }
+}
+
+# Associate Route Table with Subnet
+resource "aws_route_table_association" "rta" {
+  subnet_id      = aws_subnet.my_subnet.id
+  route_table_id = aws_route_table.route_table.id
+}
+
+# Create Security Group in the VPC
+resource "aws_security_group" "web_sg" {
+  name        = "web-sg"
+  description = "Allow SSH and HTTP"
+  vpc_id      = aws_vpc.my_vpc.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "web-sg"
+  }
+}
+
+# Launch EC2 instance in the same subnet & VPC
+resource "aws_instance" "web" {
+  ami                    = "ami-0f5ee92e2d63afc18"  # Amazon Linux 2 in ap-south-1
+  instance_type          = "t2.micro"
+  subnet_id              = aws_subnet.my_subnet.id
+  vpc_security_group_ids = [aws_security_group.web_sg.id]
+
+  tags = {
+    Name = "terraform-ec2"
+  }
+}
---
+++ output.tf
@@ -0,0 +1,15 @@
+output "vpc_id" {
+  value = aws_vpc.my_vpc.id
+}
+
+output "subnet_id" {
+  value = aws_subnet.my_subnet.id
+}
+
+output "security_group_id" {
+  value = aws_security_group.web_sg.id
+}
+
+output "ec2_public_ip" {
+  value = aws_instance.web.public_ip
+}
