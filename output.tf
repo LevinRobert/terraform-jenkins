e376e996d76e6dfa542f91d4f162b6904f826c53
LevinRobert <levin16robert@gmail.com>
2026-01-11 21:25:47 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,17 +1,19 @@
 output "vpc_id" {
-  value = aws_vpc.my_vpc.id
+  description = "VPC ID"
+  value       = aws_vpc.my_vpc.id
 }
 
 output "subnet_id" {
-  value = aws_subnet.my_subnet.id
+  description = "Subnet ID"
+  value       = aws_subnet.my_subnet.id
 }
 
 output "security_group_id" {
-  value = aws_security_group.web_sg.id
+  description = "Security Group ID"
+  value       = aws_security_group.web_sg.id
 }
 
-output "ec2_public_ip" {
-  value = aws_instance.web.public_ip
+output "ec2_public_ips" {
+  description = "Public IPs of all EC2 instances"
+  value       = aws_instance.web[*].public_ip
 }
-
-
