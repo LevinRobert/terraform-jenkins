5b5fa5ee8a118b300e470f2cd3be6d593f651120
LevinRobert <levin16robert@gmail.com>
2026-01-11 21:12:38 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -87,3 +87,25 @@ resource "aws_instance" "web" {
     Name = "terraform-ec2"
   }
 }
+
+resource "aws_instance" "web" {
+  ami                    = "ami-0f5ee92e2d63afc18"  # Amazon Linux 2 in ap-south-1
+  instance_type          = "t2.micro"
+  subnet_id              = aws_subnet.my_subnet.id
+  vpc_security_group_ids = [aws_security_group.web_sg.id]
+
+  tags = {
+    Name = "ansible"
+  }
+}
+
+resource "aws_instance" "web" {
+  ami                    = "ami-0f5ee92e2d63afc18"  # Amazon Linux 2 in ap-south-1
+  instance_type          = "t2.micro"
+  subnet_id              = aws_subnet.my_subnet.id
+  vpc_security_group_ids = [aws_security_group.web_sg.id]
+
+  tags = {
+    Name = "ansible"
+  }
+}
