c5783cfa0fe4492750056f6f235fdbb01446bd2b
LevinRobert <levin16robert@gmail.com>
2026-01-18 16:26:44 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -84,6 +84,7 @@ resource "aws_instance" "web" {
   instance_type          = "t2.medium"
   subnet_id              = aws_subnet.my_subnet.id
   vpc_security_group_ids = [aws_security_group.web_sg.id]
+  associate_public_ip_address = true
 
   tags = {
     Name = "ansible"
